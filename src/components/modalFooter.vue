e92494ee497b23d455aad17c9d1f6914df1f60b7
zhongchengjie <465152138@qq.com>
2017-12-08 18:01:56 +0800

    add vue-js-modal

---
+++ src/components/modalFooter.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="modal-header">
+		<span class="pull-right">关闭</span>
+	    <h5>{{modalTitle}}</h5>
+	</div>
+</template>
+
+<script>
+export default {
+	  name:"modalHeader",
+	  props:["modalTitle"],
+	  data:function(){
+	  	return{
+	  	}
+	  },
+	  destroyed:function(){
+	  },
+	  methods:{
+	  }
+}
+</script>
---
+++ src/components/modalHeader.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="modal-header">
+		<span class="pull-right"><i class="fa fa-remove"></i></span>
+	    <h5>{{modalTitle}}</h5>
+	</div>
+</template>
+
+<script>
+export default {
+	  name:"modalHeader",
+	  props:["modalTitle"],
+	  data:function(){
+	  	return{
+	  	}
+	  },
+	  destroyed:function(){
+	  },
+	  methods:{
+	  }
+}
+</script>
+
+<style scoped>
+</style>
